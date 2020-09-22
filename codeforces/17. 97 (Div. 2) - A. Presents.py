0a0f860b5262767ca6806364c63fa06e17e02338
Safiul Kabir <safiulanik@gmail.com>
2020-09-22 23:46:21 +0600

    Add 97 (Div. 2) - A. Presents.py

---
+++ codeforces/17. 97 (Div. 2) - A. Presents.py
@@ -0,0 +1,12 @@
+"""
+URL: https://codeforces.com/problemset/problem/136/A
+Author: safiulanik at gmail.com
+"""
+
+n = int(input())
+inp = list(map(int, input().split()))
+outp = [0] * n
+for i in range(n):
+    outp[inp[i] - 1] = i + 1
+
+print(' '.join(map(str, outp)))
