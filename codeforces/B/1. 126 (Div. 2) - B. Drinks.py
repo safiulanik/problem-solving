4dbef9ad265100fc5815a7b02c46729ce7612c33
Safiul Kabir <safiulanik@gmail.com>
2020-10-04 23:46:45 +0600

    Add 1. 126 (Div. 2) - B. Drinks.py
    Also add folder for A problems
    Also add folder for B problems

---
+++ codeforces/B/1. 126 (Div. 2) - B. Drinks.py
@@ -0,0 +1,8 @@
+"""
+URL: https://codeforces.com/problemset/problem/200/B
+Author: Safiul Kabir [safiulanik at gmail.com]
+Tags: implementation, math, *800
+"""
+
+n = int(input())
+print(sum(map(int, input().split()))/n)
