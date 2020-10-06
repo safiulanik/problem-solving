4db2d170bab40cd5fa2577edee437d3cad9fbd83
Safiul Kabir <safiulanik@gmail.com>
2020-10-06 22:59:26 +0600

    Add 31. 675 (Div. 2) - A. Fence.py

---
+++ codeforces/A/31. 675 (Div. 2) - A. Fence.py
@@ -0,0 +1,10 @@
+"""
+URL: https://codeforces.com/problemset/problem/1374/C
+Author: Safiul Kabir [safiulanik at gmail.com]
+Tags: geometry, math, *800
+"""
+
+t = int(input())
+for _ in range(t):
+    a, b, c = map(int, input().split())
+    print(max(a, b, c) + 1)
