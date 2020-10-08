468a3387f14c05ab0be5a8a14e45c8844c19de21
Safiul Kabir <safiulanik@gmail.com>
2020-10-08 23:38:37 +0600

    Add 32. 633 (Div. 2) - A. Filling Diamonds.py

---
+++ codeforces/A/32. 633 (Div. 2) - A. Filling Diamonds.py
@@ -0,0 +1,9 @@
+"""
+URL: https://codeforces.com/problemset/problem/1339/A
+Author: Safiul Kabir [safiulanik at gmail.com]
+Tags: brute force, db, implementation, math, *900
+"""
+
+t = int(input())
+for _ in range(t):
+    print(input())
