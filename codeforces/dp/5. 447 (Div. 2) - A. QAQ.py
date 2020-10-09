8f50e426601f541aa46b27e2d2bfa8558e128f59
Safiul Kabir <safiulanik@gmail.com>
2020-10-09 23:32:29 +0600

    Add 5. 447 (Div. 2) - A. QAQ.py

--- codeforces/dp/5. 447 (Div. 2) - A. QAQ.py
+++ codeforces/dp/5. 447 (Div. 2) - A. QAQ.py
@@ -4,26 +4,16 @@ Author: Safiul Kabir [safiulanik at gmail.com]
 Tags: brute force, dp, *800
 """
 
-
-def count_QAQ(s, count):
-    for i in range(len(s) - 2):
-        if s[i] == 'Q':
-            if s[i + 1] != 'A':
-                count_QAQ(f'Q{s[i + 2:]}', count)
-            else:
-                # if s[i+2] != 'Q':
-                if s[i + 2] == 'Q':
-                    count += 1
-                count_QAQ(f'QA{s[i + 3:]}', count)
-    return count
-
-
 s = input()
 count = 0
-tmp_s = ''
-for c in s:
-    if c in 'QA':
-        tmp_s += c
-s = tmp_s
-
-print(count_QAQ(s, count))
+for i in range(len(s)):
+    if s[i] == 'A':
+        a, b = 0, 0
+        for j in range(i - 1, -1, -1):
+            if s[j] == 'Q':
+                a += 1
+        for j in range(i + 1, len(s)):
+            if s[j] == 'Q':
+                b += 1
+        count += a * b
+print(count)
