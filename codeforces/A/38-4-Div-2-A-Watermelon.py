894e55c94a417ca269b1adc06506121f846bf654
Safiul Kabir <safiulanik@gmail.com>
2020-10-19 00:05:57 +0600

    Add 38-4-Div-2-A-Watermelon.py

---
+++ codeforces/A/38-4-Div-2-A-Watermelon.py
@@ -0,0 +1,11 @@
+"""
+URL: https://codeforces.com/problemset/problem/4/A
+Author: Safiul Kabir [safiulanik at gmail.com]
+Tags: brute force, math, *800
+"""
+
+n = int(input())
+if n % 2 == 1 or n == 2:
+    print("NO")
+else:
+    print("YES")
