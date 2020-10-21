7a57cbba0c567adf305f54d143bae9fd2325560b
Safiul Kabir <safiulanik@gmail.com>
2020-10-21 16:28:42 +0600

    Add 39-677-Div-3-A-Boring-Apartments.py

---
+++ codeforces/A/39-677-Div-3-A-Boring-Apartments.py
@@ -0,0 +1,14 @@
+"""
+Author: Safiul Kabir [safiulanik at gmail.com]
+Tags: implementation, math
+"""
+
+t = int(input())
+
+for _ in range(t):
+    n = input()
+    count = 10 * (int(n[0]) - 1)
+    ll = len(n)
+    for i in range(1, ll + 1):
+        count += i
+    print(count)
