ef52e7d7970eac1764b30de6b52a1a95b9a631fb
Safiul Kabir <safiulanik@gmail.com>
2021-08-03 23:57:08 +0600

    Add 2-linked-list-cycle-II.py

--- LeetCode/linked-list/2-linked-list-cycle-II.py
+++ LeetCode/linked-list/2-linked-list-cycle-II.py
@@ -1,9 +1,9 @@
 """
-URL: https://leetcode.com/explore/learn/card/linked-list/214/two-pointer-technique/1212/
+URL: https://leetcode.com/explore/learn/card/linked-list/214/two-pointer-technique/1214/
 
 Problem Statement:
 ------------------
-Given head, the head of a linked list, determine if the linked list has a cycle in it.
+Given a linked list, return the node where the cycle begins. If there is no cycle, return null.
 
 There is a cycle in a linked list if there is some node in the list that can be reached again by continuously following the next pointer. Internally, pos is used to denote the index of the node that tail's next pointer is connected to. Note that pos is not passed as a parameter.
 
@@ -11,17 +11,17 @@ Return true if there is a cycle in the linked list. Otherwise, return false.
 
 Example 1:
 Input: head = [3,2,0,-4], pos = 1
-Output: true
-Explanation: There is a cycle in the linked list, where the tail connects to the 1st node (0-indexed).
+Output: tail connects to node index 1
+Explanation: There is a cycle in the linked list, where tail connects to the second node.
 
 Example 2:
 Input: head = [1,2], pos = 0
-Output: true
-Explanation: There is a cycle in the linked list, where the tail connects to the 0th node.
+Output: tail connects to node index 0
+Explanation: There is a cycle in the linked list, where tail connects to the first node.
 
 Example 3:
 Input: head = [1], pos = -1
-Output: false
+Output: no cycle
 Explanation: There is no cycle in the linked list.
 
 Constraints:
@@ -32,6 +32,8 @@ pos is -1 or a valid index in the linked-list.
 Follow up: Can you solve it using O(1) (i.e. constant) memory?
 """
 # Definition for singly-linked list.
+
+
 class ListNode:
     def __init__(self, x):
         self.val = x
@@ -39,16 +41,22 @@ class ListNode:
 
 
 class Solution:
-    def hasCycle(self, head: ListNode) -> bool:
-        pointer1, pointer2 = head, head
-        while pointer2 is not None:
-            if pointer2.next is not None:
-                pointer2 = pointer2.next.next
-                pointer1 = pointer1.next
-                if pointer1 == pointer2:
-                    return True
-            else:
-                pointer2 = pointer2.next
+    def detectCycle(self, head: ListNode) -> ListNode:
+        current_node = head
+        while current_node != None:
+            if self.isVisitedNode(head, current_node.next, current_node):
+                return current_node.next
+            current_node = current_node.next
+        return None
+
+    def isVisitedNode(self, head, node, limit: ListNode) -> bool:
+        if head == node or node == limit:
+            return True
+        current_node = head
+        while current_node != limit:
+            if current_node == node:
+                return True
+            current_node = current_node.next
         return False
 
 
@@ -60,11 +68,8 @@ input_list = [
     [[3, 2, 0, -4], 1],
     [[1, 2], 0],
     [[1], -1],
-]
-output_list = [
-    True,
-    True,
-    False,
+    [[1], 0],
+    [[3, 2, 0, -4], 3],
 ]
 
 for i in range(len(input_list)):
@@ -80,4 +85,6 @@ for i in range(len(input_list)):
     if input_list[i][1] != -1:
         current_node.next = nodes[input_list[i][1]]
 
-    assert Solution().hasCycle(nodes[0]) == output_list[i]
+    expected_output = None if input_list[i][1] == - \
+        1 else nodes[input_list[i][1]]
+    assert Solution().detectCycle(nodes[0]) == expected_output
