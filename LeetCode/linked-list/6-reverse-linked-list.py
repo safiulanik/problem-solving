a775cfbe3c1dbb7113b3ab32932d655283442b6d
Safiul Kabir <safiulanik@gmail.com>
2021-08-12 06:26:48 +0600

    Add new solutions; Add problem descriptions in existing solutions

--- LeetCode/linked-list/6-reverse-linked-list.py
+++ LeetCode/linked-list/6-reverse-linked-list.py
@@ -1,4 +1,31 @@
+"""
+URL: https://leetcode.com/explore/learn/card/linked-list/219/classic-problems/1205/
+
+Problem Statement:
+------------------
+Given the head of a singly linked list, reverse the list, and return the reversed list.
+
+Example 1:
+Input: head = [1,2,3,4,5]
+Output: [5,4,3,2,1]
+
+Example 2:
+Input: head = [1,2]
+Output: [2,1]
+
+Example 3:
+Input: head = []
+Output: []
+
+Constraints:
+The number of nodes in the list is the range [0, 5000].
+-5000 <= Node.val <= 5000
+
+Follow up: A linked list can be reversed either iteratively or recursively. Could you implement both?
+"""
 # Definition for singly-linked list.
+
+
 class ListNode:
     def __init__(self, val=0, next=None):
         self.val = val
@@ -18,7 +45,13 @@ class Solution:
         #     head.next = old_head
         # return head
 
-        """Approach 2: Using recursion"""
+        """Approach 2: Using pythonic loop"""
+        # cur, prev = head, None
+        # while cur is not None:
+        #     cur.next, cur, prev = prev, cur.next, cur
+        # return prev
+
+        """Approach 3: Using recursion"""
         if head is None:
             return head
         if node is None:
---
+++ LeetCode/linked-list/9-palindrome-linked-list.py
@@ -0,0 +1,70 @@
+"""
+URL: https://leetcode.com/explore/learn/card/linked-list/219/classic-problems/1209/
+
+Problem Statement:
+------------------
+Given the head of a singly linked list, return true if it is a palindrome.
+
+Example 1:
+Input: head = [1,2,2,1]
+Output: true
+
+Example 2:
+Input: head = [1,2]
+Output: false
+
+Constraints:
+The number of nodes in the list is in the range [1, 105].
+0 <= Node.val <= 9
+
+Follow up: Could you do it in O(n) time and O(1) space?
+"""
+# Definition for singly-linked list.
+from typing import Optional
+
+
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+
+class Solution:
+
+    def isPalindrome(self, head: Optional[ListNode]) -> bool:
+        fast = slow = head
+        while fast.next and fast.next.next:
+            fast = fast.next.next
+            if fast and fast.next:
+                # reverse current slow node
+                old_head = head
+                head = slow.next
+                slow.next = slow.next.next
+                head.next = old_head
+            else:
+                slow = slow.next
+
+        if fast:
+            slow = slow.next
+
+        while head and slow:
+            if head.val != slow.val:
+                return False
+            head, slow = head.next, slow.next
+
+        return True
+
+
+"""
+Testing:
+--------
+"""
+head = ListNode(1)
+head.next = ListNode(2)
+head.next.next = ListNode(1)
+head.next.next.next = ListNode(2)
+head.next.next.next.next = ListNode(1)
+assert Solution().isPalindrome(head=head) == True
+while head is not None:
+    print(head.val, end=', ')
+    head = head.next
